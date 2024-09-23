8a8fa86a45b4e80c79ac934416de84732ce99d03
Gouasmia Ismael <gouasmiaismael@yahoo.fr>
2024-09-23 22:07:14 +0200

    Add mailer with gmail

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -9,8 +13,33 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app.home')]
-    public function home(): Response
+    public function home(Request $request, MailerInterface $mailer): Response
     {
-        return $this->render('home/index.html.twig');
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $email = (new Email())
+                ->from($data['email'])
+                ->to('ismaelgouasmia@gmail.com')
+                ->subject('Message du Portfolio')
+                ->text(
+                    'Nom :'.$data['name']."\n".
+                    'Email :'.$data['email']."\n\n".
+                    'Numéro :'.$data['phoneNumber']."\n\n\n".
+                    $data['message']
+                );
+
+            $mailer->send($email);
+            $this->addFlash('success', 'Message envoyé.');
+
+            return $this->redirectToRoute('app.home');
+        }
+
+        return $this->render('home/index.html.twig', [
+            'contactForm' => $form,
+        ]);
     }
 }
--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -2,29 +2,50 @@
 
 namespace App\Form;
 
-use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ContactType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class)
-            ->add('email', EmailType::class)
-            ->add('message', TextareaType::class)
+            ->add('name', TextType::class, [
+                'required' => true,
+                'label' => 'Nom · 姓名', 
+                'attr' => ['placeholder' => 'Enter your name'], 
+                //'attr' => ['class' => 'file-input'],         
+            ])
+            ->add('email', EmailType::class, [
+                'required' => true,
+                'label' => 'Email · 电子邮件',
+                'attr' => ['placeholder' => 'Enter your email'], 
+            ])
+            ->add('phoneNumber', TextType::class, [
+                'label' => 'Numéro · 数字',
+                'attr' => ['placeholder' => '+33 ou 06/07'], 
+                'constraints' => [
+                    new Regex([
+                        'pattern' => '/^(\+33|0)[1-9]([-. ]?[0-9]{2}){4}$/',
+                        'message' => 'Veuillez entrer un numéro de téléphone valide.',
+                    ]),
+                ],
+            ])
+            ->add('message', TextareaType::class, [
+                'required' => true,
+                'label' => 'Message · 信息',
+                'attr' => ['placeholder' => 'Enter your message'], 
+            ])
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
-        $resolver->setDefaults([
-            'data_class' => Contact::class,
-        ]);
+        $resolver->setDefaults([]);
     }
 }
